3ce163add4b8417f6a1e5d9d85a0bee5081c936e
Ocelot5836 <FinntheRaider@gmail.com>
2018-12-30 09:36:32 -0800

    formatting

--- src/main/java/com/zerra/client/view/WindowManager.java
+++ src/main/java/com/zerra/client/view/WindowManager.java
@@ -1,98 +1,99 @@
 package com.zerra.client.view;
 
-import com.zerra.client.Zerra;
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_ESCAPE;
+import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
+import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
+import static org.lwjgl.glfw.GLFW.GLFW_TRUE;
+import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
+import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
+import static org.lwjgl.glfw.GLFW.glfwDestroyWindow;
+import static org.lwjgl.glfw.GLFW.glfwGetPrimaryMonitor;
+import static org.lwjgl.glfw.GLFW.glfwGetVideoMode;
+import static org.lwjgl.glfw.GLFW.glfwGetWindowSize;
+import static org.lwjgl.glfw.GLFW.glfwInit;
+import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
+import static org.lwjgl.glfw.GLFW.glfwSetErrorCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetKeyCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowPos;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowShouldClose;
+import static org.lwjgl.glfw.GLFW.glfwShowWindow;
+import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
+import static org.lwjgl.glfw.GLFW.glfwTerminate;
+import static org.lwjgl.glfw.GLFW.glfwWindowHint;
+
+import java.nio.IntBuffer;
+
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.glfw.GLFWVidMode;
 import org.lwjgl.system.MemoryStack;
 import org.lwjgl.system.MemoryUtil;
 
-import java.nio.IntBuffer;
-
-import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
-import static org.lwjgl.glfw.GLFW.*;
+import com.zerra.client.Zerra;
 
 public class WindowManager {
 
-    private static long window;
-    private static GLFWErrorCallback errorCallback;
+	private static long window;
+	private static GLFWErrorCallback errorCallback;
 
-    public static long getWindow() {
-        return window;
-    }
+	public static long getWindow() {
+		return window;
+	}
 
-    public static void init() {
+	public static void init() {
+		errorCallback = GLFWErrorCallback.createPrint(System.err);
+		errorCallback.set();
 
-        errorCallback = GLFWErrorCallback.createPrint(System.err);
-        errorCallback.set();
+		if (!glfwInit()) {
+			throw new IllegalStateException("Failed to initialize GLFW");
+		}
 
-        if (!glfwInit()) {
-            throw new IllegalStateException("Failed to initialize GLFW");
-        }
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+		glfwWindowHint(GLFW_VISIBLE, GLFW_TRUE);
 
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
-        glfwWindowHint(GLFW_VISIBLE, GLFW_TRUE);
+		window = glfwCreateWindow(800, 600, "Zerra", MemoryUtil.NULL, MemoryUtil.NULL);
 
-        window = glfwCreateWindow(800, 600, "Zerra", MemoryUtil.NULL, MemoryUtil.NULL);
+		if (window == MemoryUtil.NULL) {
+			throw new IllegalStateException("Failed to create window!");
+		}
 
-        if (window == MemoryUtil.NULL) {
-            throw new IllegalStateException("Failed to create window!");
-        }
+		glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+			Zerra.schedule(genRunnable(window, key, scancode, action, mods));
+		});
 
-        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-            Zerra.schedule(genRunnable(window, key, scancode, action, mods));
-        });
+		try (MemoryStack stack = MemoryStack.stackPush()) {
+			IntBuffer pWidth = stack.mallocInt(1); // int*
+			IntBuffer pHeight = stack.mallocInt(1); // int*
 
-        try (MemoryStack stack = MemoryStack.stackPush()) {
-            IntBuffer pWidth = stack.mallocInt(1); // int*
-            IntBuffer pHeight = stack.mallocInt(1); // int*
+			glfwGetWindowSize(window, pWidth, pHeight);
 
-            glfwGetWindowSize(window, pWidth, pHeight);
+			GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
 
-            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+			glfwSetWindowPos(window, (vidmode.width() - pWidth.get(0)) / 2, (vidmode.height() - pHeight.get(0)) / 2);
+		}
+		glfwMakeContextCurrent(window);
 
-            glfwSetWindowPos(
-                    window,
-                    (vidmode.width() - pWidth.get(0)) / 2,
-                    (vidmode.height() - pHeight.get(0)) / 2
-            );
-        }
-        glfwMakeContextCurrent(window);
+		glfwSwapInterval(1);
 
-        glfwSwapInterval(1);
+		glfwShowWindow(window);
+	}
 
-        glfwShowWindow(window);
-    }
+	private static Runnable genRunnable(long window, int key, int scancode, int action, int mods) {
+		if (action == GLFW_RELEASE) {
+			switch (key) {
+			case GLFW_KEY_ESCAPE:
+				return () -> glfwSetWindowShouldClose(window, true);
+			}
+		}
+		return () -> {
+		};
+	}
 
-    private static Runnable genRunnable(long window, int key, int scancode, int action, int mods) {
-        if (action == GLFW_RELEASE) {
-            switch (key) {
-                case GLFW_KEY_ESCAPE:
-                    return new Runnable() {
-
-                        @Override
-                        public void run() {
-                            glfwSetWindowShouldClose(window, true);
-                        }
-
-                    };
-            }
-
-        }
-        return new Runnable() {
-
-            @Override
-            public void run() {
-
-            }
-
-        };
-    }
-
-    public static void exit() {
-        errorCallback.close();
-        glfwFreeCallbacks(window);
-        glfwDestroyWindow(window);
-        glfwTerminate();
-        glfwSetErrorCallback(null).free();
-    }
+	public static void exit() {
+		errorCallback.close();
+		glfwFreeCallbacks(window);
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		glfwSetErrorCallback(null).free();
+	}
 }
