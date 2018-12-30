d30281baf3db9dbefb576654993b328c94f4c8ef
Ocelot5836 <FinntheRaider@gmail.com>
2018-12-30 10:06:56 -0800

    switched to new display

--- src/main/java/com/zerra/client/Zerra.java
+++ src/main/java/com/zerra/client/Zerra.java
@@ -1,26 +1,71 @@
 package com.zerra.client;
 
-import com.zerra.client.view.WindowManager;
-
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+import org.apache.commons.lang3.Validate;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.zerra.client.view.Display;
 
 public class Zerra implements Runnable {
 
-	//3 threads; 1 for networking, 1 for rendering, 1 for gameloop
-	ExecutorService executorService = Executors.newFixedThreadPool(3);
-	
+	private static final Logger LOGGER = LogManager.getLogger();
+
+	private static Zerra instance;
+	private ExecutorService pool;
+	private ScheduledExecutorService loop;
+
 	public Zerra() {
-		
+		instance = this;
+		this.pool = Executors.newCachedThreadPool();
+		this.loop = Executors.newSingleThreadScheduledExecutor();
 	}
 
-	public static void schedule(Runnable genRunnable) {
-		//TODO: implement
+	public static void schedule(Runnable runnable) {
+		Validate.notNull(runnable);
 	}
 
 	@Override
 	public void run() {
-		WindowManager.init();
+		Display.createDisplay("Zerra", 1280, 720);
+		while (!Display.isCloseRequested()) {
+			Display.update();
+		}
 		System.out.println("client");
+	}
+
+	public void onKeyPressed(int keyCode) {
+	}
+
+	public void onKeyReleased(int keyCode) {
+	}
+
+	public void onMousePressed(double mouseX, double mouseY, int mouseButton) {
+	}
+
+	public void onMouseReleased(double mouseX, double mouseY, int mouseButton) {
+	}
+
+	public void onMouseScrolled(double mouseX, double mouseY, double yoffset) {
+	}
+
+	public void stop() {
+		instance = null;
+		pool.shutdown();
+		loop.shutdown();
+	}
+
+	public void dispose() {
+	}
+
+	public static Logger logger() {
+		return LOGGER;
+	}
+
+	public static Zerra getInstance() {
+		return instance;
 	}
 }
