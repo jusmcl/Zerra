1c94ddfaaf08fe92179c934fc8e0fe38e220c75d
Ocelot5836 <FinntheRaider@gmail.com>
2019-01-03 14:25:34 -0800

    Update Launch.java

--- src/main/java/com/zerra/Launch.java
+++ src/main/java/com/zerra/Launch.java
@@ -7,7 +7,7 @@ import com.zerra.server.ZerraServer;
 public class Launch {
 
     public static final String NAME = "Zerra";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.0.2";
     public static final String DOMAIN = "zerra";
     public static final boolean IS_DEVELOPMENT_BUILD = true;
     
