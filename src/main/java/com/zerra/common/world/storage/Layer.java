801da8f0e2a481dec7d785869a6794826c71470a
Ocelot5836 <FinntheRaider@gmail.com>
2019-01-02 17:23:27 -0800

    formatting

--- src/main/java/com/zerra/common/world/storage/Layer.java
+++ src/main/java/com/zerra/common/world/storage/Layer.java
@@ -1,20 +1,19 @@
 package com.zerra.common.world.storage;
 
-import com.zerra.common.world.entity.Entity;
-import com.zerra.common.world.storage.plate.Plate;
-import com.zerra.common.world.tile.Tile;
 import org.joml.Vector2i;
 import org.joml.Vector3i;
 
-import java.io.File;
+import com.zerra.common.world.entity.Entity;
+import com.zerra.common.world.storage.plate.Plate;
+import com.zerra.common.world.tile.Tile;
 
 public interface Layer {
 
-    Tile getTileAt(Vector2i position, int y);
+	Tile getTileAt(Vector2i position, int y);
 
-    Plate[] getLoadedPlates();
+	Plate[] getLoadedPlates();
 
-    Entity[] getEntities();
+	Entity[] getEntities();
 
-    Plate getPlate(Vector3i pos);
+	Plate getPlate(Vector3i pos);
 }
