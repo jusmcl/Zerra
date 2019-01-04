b84792357bcba3d6762053fd92493fc00b56798d
Tebreca <ewan.arends10@gmail.com>
2019-01-04 20:26:01 +0100

    work on a not useless class

--- src/main/java/com/zerra/common/world/storage/IOManager.java
+++ src/main/java/com/zerra/common/world/storage/IOManager.java
@@ -2,6 +2,7 @@ package com.zerra.common.world.storage;
 
 import com.zerra.common.ArgsBuilder;
 import com.zerra.common.CrashCodes;
+import com.zerra.common.world.World;
 
 import javax.annotation.Nonnull;
 import java.io.File;
@@ -20,7 +21,7 @@ public class IOManager {
         return instanceDir;
     }
 
-    public static void init(@Nonnull File folder){
+    public static void init(@Nonnull File folder) {
         saves = new File(folder.getAbsolutePath() + "saves/");
         if (!saves.exists()) {
             ArgsBuilder.LAUNCH.error("The Folder " + saves.getAbsolutePath() + " doesn't exist!!");
@@ -29,5 +30,18 @@ public class IOManager {
         instanceDir = folder;
     }
 
+
+    public static class WorldStorageManager {
+
+        private final World world;
+
+        public WorldStorageManager(World world) {
+            this.world = world;
+        }
+
+        public World getWorld() {
+            return world;
+        }
+    }
 
 }
