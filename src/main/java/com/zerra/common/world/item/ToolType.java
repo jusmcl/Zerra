c330b1415e3d9e5620bf4ed3917f5c5b81030518
Arpaesis <arpaesis@gmail.com>
2019-01-06 16:20:12 -0500

    Did some changes to tool and tile types.

---
+++ src/main/java/com/zerra/common/world/item/ToolType.java
@@ -0,0 +1,17 @@
+package com.zerra.common.world.item;
+
+public class ToolType
+{
+
+	private String name;
+
+	public ToolType(String name)
+	{
+		this.name = name;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+}
---
+++ src/main/java/com/zerra/common/world/tile/ZerraTileTypes.java
@@ -0,0 +1,11 @@
+package com.zerra.common.world.tile;
+
+import com.zerra.common.world.item.ZerraToolTypes;
+
+public class ZerraTileTypes {
+
+	public static final TileType AIR = new TileType("type_air", 0f, 0f, ZerraToolTypes.NONE);
+	public static final TileType STONE = new TileType("type_stone", 2f, 2f, ZerraToolTypes.SHOVEL);
+    public static final TileType GRASS = new TileType("type_grass", 1f, 1f, ZerraToolTypes.SHOVEL);
+    public static final TileType SAND = new TileType("type_sand", 1f, 1f, ZerraToolTypes.SHOVEL);
+}
