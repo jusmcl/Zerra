90877e8aaf250712d3d20391afb8226a3081938d
Arpaesis <arpaesis@gmail.com>
2019-01-07 20:17:26 -0500

    Update Item.java

--- src/main/java/com/zerra/common/world/item/Item.java
+++ src/main/java/com/zerra/common/world/item/Item.java
@@ -1,8 +1,22 @@
 package com.zerra.common.world.item;
 
-public abstract class Item {
+import com.zerra.common.world.World;
+import com.zerra.common.world.entity.Entity;
 
-    public abstract String getID();
+public class Item
+{
+	public Item(String registryName)
+	{
 
-    
+	}
+
+	public boolean onLeftClickEntity(Entity entity, World world)
+	{
+		return true;
+	}
+
+	public boolean onRightClickEntity(Entity entity, World world)
+	{
+		return true;
+	}
 }
