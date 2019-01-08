7def21f1382f20975b86ced17a33962ec5b38476
Arpaesis <arpaesis@gmail.com>
2019-01-07 22:01:25 -0500

    More work on items.

--- src/main/java/com/zerra/common/world/item/Item.java
+++ src/main/java/com/zerra/common/world/item/Item.java
@@ -2,20 +2,48 @@ package com.zerra.common.world.item;
 
 import com.zerra.common.world.World;
 import com.zerra.common.world.entity.Entity;
+import com.zerra.common.world.tile.Tile;
 
 public class Item
 {
+	private String registryName;
+
 	public Item(String registryName)
 	{
-
+		this.registryName = registryName;
 	}
 
-	public boolean onLeftClickEntity(Entity entity, World world)
+	public String getRegistryName()
+	{
+		return registryName;
+	}
+
+	public boolean onLeftClick(World world, ItemGroup group)
 	{
 		return true;
 	}
 
-	public boolean onRightClickEntity(Entity entity, World world)
+	public boolean onRightClick(World world, ItemGroup group)
+	{
+		return true;
+	}
+
+	public boolean onLeftClickEntity(Entity entity, World world, ItemGroup group)
+	{
+		return true;
+	}
+
+	public boolean onRightClickEntity(Entity entity, World world, ItemGroup group)
+	{
+		return true;
+	}
+
+	public boolean onLeftClickTile(Tile tile, World world, ItemGroup group)
+	{
+		return true;
+	}
+
+	public boolean oRightClickTile(Tile tile, World world, ItemGroup group)
 	{
 		return true;
 	}
