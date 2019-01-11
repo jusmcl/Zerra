013271773a368d557bc1c4b34b07808cbb2418ee
Arpaesis <arpaesis@gmail.com>
2019-01-10 20:15:52 -0500

    Added a menu state.

---
+++ src/main/java/com/zerra/client/MenuState.java
@@ -0,0 +1,17 @@
+package com.zerra.client;
+
+public class MenuState extends State
+{
+
+	@Override
+	public void update()
+	{
+		//TODO: Update menu!
+	}
+
+	@Override
+	public void render()
+	{
+		//TODO: Render a background and buttons here!
+	}
+}
