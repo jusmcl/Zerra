c0ec69a44d22bfca46f5d11d8b63b66befd5be44
Arpaesis <arpaesis@gmail.com>
2019-01-12 15:16:26 -0500

    Improved mod loading efficiency.

--- src/main/java/com/zerra/api/mod/ModLoader.java
+++ src/main/java/com/zerra/api/mod/ModLoader.java
@@ -1,14 +1,11 @@
 package com.zerra.api.mod;
 
-import java.io.File;
-import java.net.URL;
-import java.net.URLClassLoader;
+import java.util.Set;
 
 import org.reflections.Reflections;
-import org.reflections.scanners.SubTypesScanner;
 
-import com.google.common.collect.Iterables;
 import com.zerra.client.Zerra;
+import com.zerra.common.util.MiscUtils;
 
 public class ModLoader
 {
@@ -21,39 +18,30 @@ public class ModLoader
 
 	public void loadMods(String directory)
 	{
-		File modDir = new File(directory);
-
 		long snapshot = System.currentTimeMillis();
 
-		for (File jar : modDir.listFiles())
-		{
+		Reflections reflections = new Reflections();
 
+		Set<Class<? extends Mod>> loadedMods = reflections.getSubTypesOf(Mod.class);
+
+		for (Class<? extends Mod> clazz : loadedMods)
+		{
+			Mod mod = null;
 			try
 			{
-				System.out.println("Attempting to load " + jar.getName());
-
-				Reflections reflections = new Reflections(new URLClassLoader(new URL[]
-				{ jar.toURI().toURL() }, ModLoader.class.getClassLoader()), new SubTypesScanner());
-
-				Class<? extends Mod> mod = Iterables.getFirst(reflections.getSubTypesOf(Mod.class), null);
-
-				if (mod != null)
-				{
-					Mod instance = mod.getConstructor().newInstance();
-
-					modManager.loadedMods.put(instance.getModInfo().getDomain(), instance);
-				} else
-				{
-					Zerra.logger().error("Failed to load " + jar.getName() + " , mod does not have a valid class implementing Mod.class. The mod will not be loaded.");
-				}
+				mod = clazz.getConstructor().newInstance();
 			} catch (Exception e)
 			{
 				e.printStackTrace();
 			}
+
+			if (mod != null)
+			{
+				modManager.loadedMods.put(mod.getModInfo().getDomain(), mod);
+			}
 		}
-		
-		long calc = (System.currentTimeMillis() - snapshot) / 1000;
-		Zerra.logger().info("Finished loading mods in " + calc + " seconds.");
+
+		Zerra.logger().info("Finished loading mods in " + MiscUtils.secondsSinceTime(snapshot));
 
 	}
 }
