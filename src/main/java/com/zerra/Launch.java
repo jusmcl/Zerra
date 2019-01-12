133bd0d1e4b8a56870972c7154dd845c396ea01b
Arpaesis <arpaesis@gmail.com>
2019-01-11 23:34:14 -0500

    Updated zerra version.

--- src/main/java/com/zerra/Launch.java
+++ src/main/java/com/zerra/Launch.java
@@ -17,7 +17,7 @@ public class Launch
 {
 
 	public static final String NAME = "Zerra";
-	public static final String VERSION = "0.0.3";
+	public static final String VERSION = "0.0.4";
 	public static final String DOMAIN = "zerra";
 	public static final boolean IS_DEVELOPMENT_BUILD = true;
 
