bb40baa802457290d71d7203afe2c8e9f777e8f6
Arpaesis <arpaesis@gmail.com>
2019-01-12 12:58:39 -0500

    Slight improvements to mod setup.

--- src/main/java/com/zerra/api/mod/ExampleMod.java
+++ src/main/java/com/zerra/api/mod/ExampleMod.java
@@ -1,28 +1,29 @@
 package com.zerra.api.mod;
 
-import com.zerra.api.mod.Mod;
 import com.zerra.api.mod.info.ModInfo;
 import com.zerra.api.mod.info.ModInfoBuilder;
 
 public class ExampleMod implements Mod
 {
 
+	ModInfoBuilder builder;
+	
 	@Override
 	public void init()
 	{
-		System.out.println("Initializing mod...");
+		this.getLogger().info("Initializing mod...");
 	}
 
 	@Override
 	public void postInit()
 	{
-		System.out.println(this.getModInfo().getModName() + " has finished initialization.");
+		this.getLogger().info(this.getModInfo().getModName() + " has finished initialization.");
 	}
 
 	@Override
 	public ModInfo getModInfo()
 	{
-		return new ModInfoBuilder()
+		return builder
 				.setAuthors("Arpaesis")
 				.setCredits("Credits to the Zerra development team for making the game!")
 				.setDependencies("dependencyDomain")
--- src/main/java/com/zerra/api/mod/Mod.java
+++ src/main/java/com/zerra/api/mod/Mod.java
@@ -1,9 +1,16 @@
 package com.zerra.api.mod;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.zerra.api.mod.info.ModInfo;
 
 public interface Mod
 {
+	public default Logger getLogger()
+	{
+		return LogManager.getLogger(getModInfo().getModName());
+	}
 
 	public void init();
 
