290f60f1cefba0f5948e1c18676bada776b01f82
Arpaesis <arpaesis@gmail.com>
2019-01-12 15:15:26 -0500

    Changed misc util method output.

--- src/main/java/com/zerra/common/util/MiscUtils.java
+++ src/main/java/com/zerra/common/util/MiscUtils.java
@@ -3,6 +3,6 @@ package com.zerra.common.util;
 public class MiscUtils {
 
     public static String secondsSinceTime(long millis) {
-        return (float) (System.currentTimeMillis() - millis) / 1000F + "s";
+        return (float) (System.currentTimeMillis() - millis) / 1000F + " seconds.";
     }
 }
