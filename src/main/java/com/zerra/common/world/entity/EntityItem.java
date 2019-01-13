f25f32a0ab9d8f7a69a0e12d3f4e288430c09b3e
Arpaesis <arpaesis@gmail.com>
2019-01-13 18:51:35 -0500

    Item entities now despawn after 5 minutes.

--- src/main/java/com/zerra/common/world/entity/EntityItem.java
+++ src/main/java/com/zerra/common/world/entity/EntityItem.java
@@ -1,5 +1,6 @@
 package com.zerra.common.world.entity;
 
+import com.zerra.client.Zerra;
 import com.zerra.common.world.item.Item;
 import com.zerra.common.world.item.ItemGroup;
 import com.zerra.common.world.storage.plate.WorldLayer;
@@ -8,16 +9,29 @@ public class EntityItem extends Entity
 {
 	private ItemGroup group;
 
-    public EntityItem(WorldLayer worldLayer, Item item) {
-        this(worldLayer, new ItemGroup(item, 1, 0));
-    }
+	public EntityItem(WorldLayer worldLayer, Item item)
+	{
+		this(worldLayer, new ItemGroup(item, 1, 0));
+	}
 
-    public EntityItem(WorldLayer worldLayer, ItemGroup group) {
-        super(worldLayer);
+	public EntityItem(WorldLayer worldLayer, ItemGroup group)
+	{
+		super(worldLayer);
 	}
 
 	public ItemGroup getGroup()
 	{
 		return group;
+	}
+	
+	@Override
+	public void update()
+	{
+		super.update();
+		
+		if(this.getTicksExisted() >= (Zerra.getInstance().getTicksPerSecond() * 60 * 5))
+		{
+			this.despawn();
+		}
 	}
 }
