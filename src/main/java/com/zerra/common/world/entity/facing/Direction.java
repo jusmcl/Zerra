fdeb966b0f59b50094199bccacd4c5b08a2b6995
Arpaesis <arpaesis@gmail.com>
2019-01-14 18:55:16 -0500

    Added an opposite function for direction.

--- src/main/java/com/zerra/common/world/entity/facing/Direction.java
+++ src/main/java/com/zerra/common/world/entity/facing/Direction.java
@@ -2,15 +2,31 @@ package com.zerra.common.world.entity.facing;
 
 public enum Direction
 {
-	NORTH,
-	SOUTH,
-	EAST,
-	WEST;
+	NORTH, SOUTH, EAST, WEST;
 
-    private static final int SIZE = values().length;
+	private static final int SIZE = values().length;
 
-    public static Direction getFromIndex(int index) {
-        int i = index < 0 || index > SIZE ? 0 : index;
-        return values()[i];
-    }
+	public static Direction getFromIndex(int index)
+	{
+		int i = index < 0 || index > SIZE ? 0 : index;
+		return values()[i];
+	}
+
+	public Direction opposite()
+	{
+		switch (this)
+		{
+		case NORTH:
+			return Direction.SOUTH;
+		case SOUTH:
+			return Direction.NORTH;
+		case EAST:
+			return Direction.WEST;
+		case WEST:
+			return Direction.EAST;
+		default:
+			// No set direction.
+			return null;
+		}
+	}
 }
