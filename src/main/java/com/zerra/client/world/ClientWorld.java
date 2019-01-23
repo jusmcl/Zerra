a08635d68052dc52384a40e283503b766004a80f
Arpaesis <arpaesis@gmail.com>
2019-01-22 19:25:35 -0500

    Prepare for client/server worlds.

---
+++ src/main/java/com/zerra/client/world/ClientWorld.java
@@ -0,0 +1,13 @@
+package com.zerra.client.world;
+
+import com.zerra.common.world.World;
+
+public class ClientWorld extends World
+{
+
+	public ClientWorld(String name, Long seed)
+	{
+		super(name, seed);
+	}
+
+}
---
+++ src/main/java/com/zerra/server/world/ServerWorld.java
@@ -0,0 +1,13 @@
+package com.zerra.server.world;
+
+import com.zerra.common.world.World;
+
+public class ServerWorld extends World
+{
+
+	public ServerWorld(String name, Long seed)
+	{
+		super(name, seed);
+	}
+
+}
