ce97559521fead7e62349688495462c9865ba7d9
bright_spark <markpassey@btinternet.com>
2019-01-23 17:07:26 +0000

    Added mod domain validation and changed exception handling

--- src/main/java/com/zerra/api/mod/ModLoader.java
+++ src/main/java/com/zerra/api/mod/ModLoader.java
@@ -1,12 +1,13 @@
 package com.zerra.api.mod;
 
-import java.util.List;
-
 import com.zerra.client.ZerraClient;
+import com.zerra.common.Zerra;
 import com.zerra.common.util.MiscUtils;
-
 import io.github.classgraph.ClassGraph;
 import io.github.classgraph.ScanResult;
+
+import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * <em><b>Copyright (c) 2019 The Zerra Team.</b></em> <br>
@@ -17,6 +18,7 @@ import io.github.classgraph.ScanResult;
  */
 public class ModLoader
 {
+	private static final Pattern DOMAIN_PATTERN = Pattern.compile("^\\w{3,}$");
 	private ModManager modManager;
 
 	public ModLoader(ModManager modManager)
@@ -43,16 +45,23 @@ public class ModLoader
 
 			for (Class<?> modClass : widgetClasses)
 			{
-				Mod mod = (Mod) modClass.getConstructor().newInstance();
-
-				if (mod != null)
+				try
 				{
-					modManager.loadedMods.put(mod.getModInfo().getDomain(), mod);
+					//TODO: Use MiscUtils.createInstance once the messages branch is merged
+					Mod mod = (Mod) modClass.getConstructor().newInstance();
+					String domain = mod.getModInfo().getDomain();
+					if(!DOMAIN_PATTERN.matcher(domain).matches())
+					{
+						throw new RuntimeException(String.format("The domain name '%s' is invalid for the pattern '%s'", domain, DOMAIN_PATTERN.pattern()));
+					}
+
+					modManager.loadedMods.put(domain, mod);
+				}
+				catch(Exception e)
+				{
+					Zerra.logger().error("Failed to load mod " + modClass.getName(), e);
 				}
 			}
-		} catch (Exception e)
-		{
-			e.printStackTrace();
 		}
 
 		ZerraClient.logger().info("Finished loading mods in " + MiscUtils.secondsSinceTime(snapshot));
