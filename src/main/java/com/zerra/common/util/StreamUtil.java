07e63ee8899d919f62270a696fd1228c1f51fc55
Arpaesis <arpaesis@gmail.com>
2019-01-26 09:23:51 -0500

    Revert "Revert "start modinfobuilder rework""
    This reverts commit ed85e60c19d07f6530ff41e3a1b079e8c1d5b8c2.

---
+++ src/main/java/com/zerra/common/util/StreamUtil.java
@@ -0,0 +1,13 @@
+package com.zerra.common.util;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class StreamUtil {
+
+    public static FileInputStream convert(InputStream Stream){
+        return null;//TODO
+    }
+
+
+}
