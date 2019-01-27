769180264a9f4273a8af2699b6f43b4d90ab7cf8
Arpaesis <arpaesis@gmail.com>
2019-01-27 17:36:42 -0500

    Bumped zerra version to 0.0.5!

--- src/main/java/com/zerra/common/Reference.java
+++ src/main/java/com/zerra/common/Reference.java
@@ -3,7 +3,7 @@ package com.zerra.common;
 public class Reference
 {
 	public static final String NAME = "Zerra";
-	public static final String VERSION = "0.0.4";
+	public static final String VERSION = "0.0.5";
 	public static final String DOMAIN = "zerra";
 	public static final boolean IS_DEVELOPMENT_BUILD = true;
 }
