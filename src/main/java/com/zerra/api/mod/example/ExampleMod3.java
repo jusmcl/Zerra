bf15d7cf2aada9900b3d8e4f26cc54cb3c9c9df1
Arpaesis <arpaesis@gmail.com>
2019-01-27 22:05:14 -0500

    Bumped zerra version to 0.0.6.

--- src/main/java/com/zerra/api/mod/example/ExampleMod3.java
+++ src/main/java/com/zerra/api/mod/example/ExampleMod3.java
@@ -9,7 +9,7 @@ import com.zerra.common.world.item.Item;
 public class ExampleMod3 implements Mod
 {
 
-	private ModInfo info = new ModInfoBuilder("examplemod3", "Example Mod 3", "1.0", "0.0.5").setAuthors("Arpaesis").setCredits("Credits to the Zerra development team for making the game!")
+	private ModInfo info = new ModInfoBuilder("examplemod3", "Example Mod 3", "1.0", "0.0.6").setAuthors("Arpaesis").setCredits("Credits to the Zerra development team for making the game!")
 			.setDependencies("exampledependency", "exampledependency2", "anotherexampledependency").setModDescription("A simple example mod.").setWebsiteURL("https://www.example.com").build();
 
 	@Override
--- src/main/java/com/zerra/api/mod/example/ExampleMod4.java
+++ src/main/java/com/zerra/api/mod/example/ExampleMod4.java
@@ -9,7 +9,7 @@ import com.zerra.common.world.item.Item;
 public class ExampleMod4 implements Mod
 {
 
-	private ModInfo info = new ModInfoBuilder("examplemod4", "Example Mod 4", "1.0", "0.0.5").setAuthors("Arpaesis").setCredits("Credits to the Zerra development team for making the game!")
+	private ModInfo info = new ModInfoBuilder("examplemod4", "Example Mod 4", "1.0", "0.0.6").setAuthors("Arpaesis").setCredits("Credits to the Zerra development team for making the game!")
 			.setDependencies("exampledependency", "exampledependency2", "anotherexampledependency").setModDescription("A simple example mod.").setWebsiteURL("https://www.example.com").build();
 
 	@Override
