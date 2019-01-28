c5ac6f29c5ae0b79c7153594c30e2948cf284490
Ocelot5836 <FinntheRaider@gmail.com>
2019-01-27 17:44:27 -0800

    formatting

--- src/main/java/com/zerra/client/gfx/ui/Button.java
+++ src/main/java/com/zerra/client/gfx/ui/Button.java
@@ -1,91 +1,56 @@
 package com.zerra.client.gfx.ui;
 
-import org.lwjgl.glfw.GLFW;
-
 import com.zerra.client.ZerraClient;
-import com.zerra.client.gfx.renderer.Renderer;
 import com.zerra.client.util.ResourceLocation;
-import com.zerra.client.view.Display;
 
 /**
  * <em><b>Copyright (c) 2019 The Zerra Team.</b></em> <br>
  * </br>
+ * 
  * UIComponent : Button
  * 
  * @author AndrewAlfazy
  */
-public class Button extends UIComponent {
+public class Button extends UIComponent
+{
+	public static final ResourceLocation BUTTONS_0 = new ResourceLocation("textures/gui/buttons.png");
+	public static final ResourceLocation BUTTONS_1 = new ResourceLocation("textures/gui/buttons2.png");
 
-	private boolean isDown, isMouseHover, sharpEdge;
+	private boolean hovered;
+	private boolean sharpEdge;
 
-	private static final ResourceLocation BUTTONS_0 = new ResourceLocation("zerra:gui/buttons.png");
-	private static final ResourceLocation BUTTONS_1 = new ResourceLocation("zerra:gui/buttons2.png");
-
-	public Button(float x, float y, float width, float height, boolean isCenterdCoords) {
-		super(x, y, width, height, isCenterdCoords);
-		isDown = false;
-		isMouseHover = false;
-		sharpEdge = true;
+	public Button(float x, float y, float width, float height, boolean isCenterdCoords)
+	{
+		this(x, y, width, height, isCenterdCoords, true);
 	}
 
-	public Button(float x, float y, float width, float height, boolean isCenterdCoords, boolean sharpEdge) {
+	public Button(float x, float y, float width, float height, boolean isCenterdCoords, boolean sharpEdge)
+	{
 		super(x, y, width, height, isCenterdCoords);
-		isDown = false;
-		isMouseHover = false;
+		this.hovered = false;
 		this.sharpEdge = sharpEdge;
 	}
 
 	@Override
-	public void render() {
-		double mouse_x = Display.getMouseX() / Renderer.SCALE;
-		double mouse_y = Display.getMouseY() / Renderer.SCALE;
-
-		if (mouse_x > x && mouse_x < x + width && mouse_y > y && mouse_y < y + height) {
-			isMouseHover = true;
-			whileHover();
-			if (ZerraClient.getInstance().getInputHandler().isMouseButtonPressed(GLFW.GLFW_MOUSE_BUTTON_1)) {
-				isDown = true;
-				whileDown();
-			} else if (isDown == true) {
-				isDown = false;
-				onClick();
-			}
-		} else {
-			isMouseHover = false;
-			isDown = false;
-		}
-
-		if (sharpEdge)
-			ZerraClient.getInstance().getRenderingManager().getTextureManager().bind(BUTTONS_1);
-		else
-			ZerraClient.getInstance().getRenderingManager().getTextureManager().bind(BUTTONS_0);
-
-		if (isDown)
-			ZerraClient.getInstance().getRenderingManager().getGuiRenderer().renderTexturedQuad(x, y, width, height, 0,
-					32, 256, 16, 256, 256);
-		else if (isMouseHover)
-			ZerraClient.getInstance().getRenderingManager().getGuiRenderer().renderTexturedQuad(x, y, width, height, 0,
-					16, 256, 16, 256, 256);
-		else
-			ZerraClient.getInstance().getRenderingManager().getGuiRenderer().renderTexturedQuad(x, y, width, height, 0,
-					0, 256, 16, 256, 256);
+	public void render(double mouseX, double mouseY, float partialTicks)
+	{
+		this.hovered = mouseX >= this.x && mouseX < this.x + this.width && mouseY >= this.y && mouseY < this.y + this.height;
+		ZerraClient.getInstance().getRenderingManager().getTextureManager().bind(this.sharpEdge ? BUTTONS_1 : BUTTONS_0);
+		ZerraClient.getInstance().getRenderingManager().getGuiRenderer().renderTexturedQuad(this.x, this.y, this.width, this.height, 0, this.hovered && ZerraClient.getInstance().getInputHandler().isMouseButtonPressed(0) ? 32 : this.hovered ? 16 : 0, 256, 16, 256, 256);
 	}
 
-	public void whileDown() {
+	@Override
+	public void mousePressed(double mouseX, double mouseY, int mouseButton)
+	{
 	}
 
-	public void onClick() {
+	@Override
+	public void mouseReleased(double mouseX, double mouseY, int mouseButton)
+	{
 	}
 
-	public void whileHover() {
+	public boolean isHovered()
+	{
+		return hovered;
 	}
-
-	public boolean isDown() {
-		return isDown;
-	}
-
-	public boolean isMouseHover() {
-		return isMouseHover;
-	}
-
 }
